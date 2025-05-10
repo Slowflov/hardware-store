0f3478dd8445dafe41299f4e89e819b295db5c09
Rouslan Ouvarov <smfdgot@gmail.com>
2025-05-10 18:29:43 +0200

    add config from connect back

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,10 @@ const PORT = process.env.PORT || 5000;
 
 async function startServer() {
   const app = express();
-  app.use(cors());
+app.use(cors({
+  origin: 'https://np-store.netlify.app',
+  credentials: true
+}));
 
   // Подключаемся к базе данных
   await connectDB();
--- frontend/src/apolloClient.jsx
+++ frontend/src/apolloClient.jsx
@@ -2,7 +2,8 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'http://localhost:5000/graphql',
+    uri: 'https://hardware-store-backend.onrender.com/graphql',
+    credentials: 'include',
   }),
   cache: new InMemoryCache(),
 });
