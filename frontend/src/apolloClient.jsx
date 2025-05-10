53e2cf4e178d14e19b9481372cf74da6eec43b30
Rouslan Ouvarov <smfdgot@gmail.com>
2025-05-10 21:08:42 +0200

    test1

--- frontend/src/apolloClient.jsx
+++ frontend/src/apolloClient.jsx
@@ -3,7 +3,6 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 const client = new ApolloClient({
   link: new HttpLink({
     uri: 'https://hardware-store-backend.onrender.com/graphql',
-    credentials: 'include',
   }),
   cache: new InMemoryCache(),
 });
